3b7c4775162861ad4171cb65597cd41e604366d0
DennisBogdanAleksandrov <extraordinarycourtesy@gmail.com>
2020-01-05 08:45:25 -0800

    .

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,17 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-// var cookieParser = require('cookie-parser');
-// var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 
 var app = express();
 
-// app.use(logger('dev'));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
-// app.use(cookieParser());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 // Serve static assets if in production
 if(process.env.NODE_ENV === 'production') {
