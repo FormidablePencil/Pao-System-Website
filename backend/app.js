0495c52ceffba8288016bd6015d09146ddfa0b86
DennisBogdanAleksandrov <extraordinarycourtesy@gmail.com>
2020-01-05 09:25:12 -0800

    .

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ const mongoose = require('mongoose');
 require('dotenv/config');
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => console.log('connected to DB!'));
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 //@ ----
